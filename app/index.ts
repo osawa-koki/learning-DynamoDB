0c67346c0ea8bb60a2bbdf38c2cd89eb45169a0f
osawa-koki <github@osawa.dev>
2023-02-27 20:57:45 +0900

    小さな修正。

--- app/index.ts
+++ app/index.ts
@@ -3,6 +3,7 @@ import AWS from "aws-sdk";
 const region = "ap-northeast-1";
 const client = new AWS.DynamoDB({ region });
 
+// テーブル一覧を取得
 client.listTables({}, (err, data) => {
   if (err) console.log(err, err.stack);
   else console.log(data);
