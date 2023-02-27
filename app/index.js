b6aa1b9e8c77d8bae2e86d5b62a72845c520d8e9
osawa-koki <github@osawa.dev>
2023-02-27 21:15:39 +0900

    項目の取得。

--- app/index.js
+++ app/index.js
@@ -8,3 +8,28 @@ client.listTables({}, (err, data) => {
   if (err) console.log(err, err.stack);
   else console.log(data);
 });
+
+const table_name = "learning-dynamodb-dynamodb";
+
+// データの挿入
+const uuidv4 = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+const user_id = uuidv4();
+const params_insert = {
+  TableName: table_name,
+  Item: {
+    user_id: { S: user_id },
+    name: { S: "test" },
+    age: { N: "20" },
+  },
+};
+
+client.putItem(params_insert, (err, data) => {
+  if (err) console.log(err, err.stack);
+  else console.log(data);
+});
