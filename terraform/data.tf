39e32670e319d2ecc6966d37ce2e15641a2f07d7
osawa-koki <github@osawa.dev>
2023-02-27 14:14:19 +0900

    データ構造変えた。

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,16 +1,35 @@
 
 resource "aws_dynamodb_table" "my-table" {
   name              = "${var.project_name}-dynamodb"
-  billing_mode      = "PAY_PER_REQUEST"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "user_id"
+  range_key      = "name"
 
   attribute {
-    name = "id"
+    name = "user_id"
     type = "S"
   }
 
-  key {
-    hash_attribute = "id"
-    type           = "HASH"
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  attribute {
+    name = "age"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name               = "name_index"
+    hash_key           = "name"
+    range_key          = "age"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["user_id"]
   }
 
   tags = {
