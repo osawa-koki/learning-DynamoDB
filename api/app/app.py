b0386fb6adc034cc79f44fca504173d0db087c73
osawa-koki <github@osawa.dev>
2023-04-06 16:38:22 +0900

    get - func

--- api/app/app.py
+++ api/app/app.py
@@ -1,9 +1,12 @@
 import json
 import logging
-import requests
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+dynamodb = boto3.client('dynamodb')
+table_name = 'learning-dynamodb-dynamodb-table'
 
 def ping(event, context):
     return {
@@ -11,6 +14,29 @@ def ping(event, context):
         "body": json.dumps(
             {
                 "message": "pong",
+            }
+        ),
+    }
+
+def get(event, context):
+
+    channel_id = event['pathParameters']['channel_id']
+
+    options = {
+        'TableName': table_name,
+        'Key': {
+            'channel_id': {
+                'S': channel_id,
+            }
+        }
+    }
+    ret = dynamodb.get_item(**options)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": ret,
             }
         ),
     }
